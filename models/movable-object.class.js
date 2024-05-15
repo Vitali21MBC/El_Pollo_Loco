110eb43d46dff45cf1cb2d4408f49b6586cecfba
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-05-15 11:29:13 +0200

    Setup constructor
    For character & chicken to load their image.

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,6 +3,11 @@ class MovableObject {
     y;
     img;
 
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
     moveRight(){
         console.log('Moving Right');
     }
