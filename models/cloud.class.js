842beff12413219263d7ca144b91366e41befc25
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-05-16 23:37:26 +0200

    New Object inserted
    background-objects and cloud added.
    code cleaned

---
+++ models/cloud.class.js
@@ -0,0 +1,13 @@
+class Cloud extends MovableObject {
+    y = 10;
+    width = 500;
+    height = 400;
+
+
+    constructor(){
+        super().loadImage('./img/5_background/layers/4_clouds/1.png');
+
+        this.x = 0 + Math.random() * 500;
+
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -5,14 +5,46 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 0, 0)
+    ];
+    canvas;
     ctx;
+
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+
     }
+
+    addObjectsToMap(objects) {
+        objects.forEach(object => {
+            this.addToMap(object);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+
+
 }
