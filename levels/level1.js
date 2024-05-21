065c0aa179b07ab0b09a6871d1924babb3a97c12
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-05-21 10:40:40 +0200

    Update
    - Set level boundries (left & right)
    - Added walking sound
    - Added endboss chicken with angry animation

--- levels/level1.js
+++ levels/level1.js
@@ -3,11 +3,16 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
+       new Endboss()
     ],
     [
         new Cloud()
     ],
     [
+        new BackgroundObject('./img/5_background/layers/air.png', -719),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', -719),
         new BackgroundObject('./img/5_background/layers/air.png', 0),
         new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 0),
@@ -16,5 +21,13 @@ const level1 = new Level(
         new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 719),
         new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 719),
         new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 719),
+        new BackgroundObject('./img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 719*2),
+        new BackgroundObject('./img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 719*3),
     ]
 );
--- models/character.class.js
+++ models/character.class.js
@@ -13,6 +13,7 @@ class Character extends MovableObject {
     ];
     world;
 
+    walking_sound = new Audio('../audio/walking.mp3')
 
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
@@ -24,25 +25,25 @@ class Character extends MovableObject {
     animate() {
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
+            this.walking_sound.pause();
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
 
-            if (this.world.keyboard.LEFT) {
+            if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
-            this.world.camera_x = -this.x;
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 //Walk animation
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
