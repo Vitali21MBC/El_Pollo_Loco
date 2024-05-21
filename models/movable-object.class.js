169fa7f2f43f5452a16f83ee136ef5bcf4241210
Vitali Rudi <159520121+Vitali21MBC@users.noreply.github.com>
2024-05-21 11:42:32 +0200

    Character Jumpf Function
    Added:
    - Gravity
    - Jump animation
    - Jump on keypress
    
    Code Optimization

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,21 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 130;
+    }
 
     loadImage(path) {
         this.img = new Image();
@@ -22,7 +37,7 @@ class MovableObject {
         });
     }
 
-    playAnimation(images){
+    playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = images[i];
         this.img = this.imageCache[path];
@@ -30,12 +45,10 @@ class MovableObject {
     }
 
     moveRight() {
-        console.log('Moving Right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
     }
 }
